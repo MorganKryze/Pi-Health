4afd73839621cc5450987f66d3ce6294dc9c760a
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-08-09 12:04:35 +0200

    🚑 quick update

--- src/ws.py
+++ src/ws.py
@@ -115,6 +115,7 @@ class Websockets:
                 await func(websocket)
             except Exception:
                 main.services_status[ws_category][ws_name] = False
+                await websocket.send_text("[WebSocket Closed]")
             finally:
                 main.services_status[ws_category][ws_name] = False
                 await websocket.send_text("[WebSocket Closed]")
